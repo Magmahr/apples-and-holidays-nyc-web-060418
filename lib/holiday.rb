eef57a9bb214e0379aff2660dcddb77fe27f74bd
Matt Maher <mattmaher0@gmail.com>
2018-05-16 18:27:55 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,7 +70,6 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday.collect do |holiday, supplies|
       if supplies == "BBQ"
-        holiday
       end 
     end 
   end  
