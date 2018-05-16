074b0e65c7eda0d09cbf4602743ce1e2ba809c2f
Matt Maher <mattmaher0@gmail.com>
2018-05-16 18:26:41 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,6 +67,9 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
+  holiday_hash.each do |season, holiday|
+    holiday.each do |holiday, supplies|
+      if supplies == ":new_years"
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 
