3c5a89454788d82e01f41de22afaaf917b7728f3
Matt Maher <mattmaher0@gmail.com>
2018-05-16 18:13:09 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,7 +53,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize!}:"
     holiday.each do |holiday, supplies|
-      if holiday.
+      if holiday.include?(_)
          
       puts "  #{holiday.to_s.capitalize!}: #{supplies.join(", ")}"
       end
